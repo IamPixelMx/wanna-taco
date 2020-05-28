84e13c7a61a29f466e094ec0c72c5185ec327c5f
Itzel Enciso <enciso.iq@gmail.com>
2020-05-28 17:43:14 -0500

    Fix axios to local request

--- redux/sagas/api/melpAPI.js
+++ redux/sagas/api/melpAPI.js
@@ -3,18 +3,9 @@ import axios from 'axios';
 const API_URL = process.env.MOCK_API_URL;
 
 export const getMelpData = async () => {
-  const melpData = await axios.get(API_URL);
+  const response = await axios.get(API_URL);
+  const melpData = response.data;
   console.log('Melp Data desde axios: ', melpData);
 
   return melpData;
 };
-
-// export const getMelpData = async () => {
-//   const response = await fetch('/__mocks__/api.js');
-//   //Entra a a data
-//   const json = await response.json();
-//   const { data } = json;
-//   console.log(data);
-
-//   return data;
-// };
