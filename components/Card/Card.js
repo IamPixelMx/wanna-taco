6bc6967151391104c491807f053334659ec5d83e
Itzel Enciso <enciso.iq@gmail.com>
2020-06-01 12:07:52 -0500

    Update rating field in card

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,6 +1,6 @@
 const Card = ({ id, rating, name, contact: { phone, site }, address: { street, city, state } }) => {
   const rateArr = [1, 2, 3, 4, 5];
-  const getStar = rate => (rate <= rating ? '⭐' : '');
+  const getStar = rate => (rate <= rating ? '&nbsp;⭐' : '');
 
   return (
     <div id={`${id}-card`} className='tile is-child box'>
@@ -17,7 +17,7 @@ const Card = ({ id, rating, name, contact: { phone, site }, address: { street, c
           <p className='subtitle is-5 is-spaced'>
             <small>📞 &nbsp; {phone} </small>
           </p>
-          <p className='is-inline-block has-text-weight-semibold'>Rating:&nbsp;</p>
+          <p className='is-inline-block has-text-weight-semibold'>Rating:&nbsp;&nbsp;{rating}</p>
           {rateArr.map(rate => getStar(rate))}
           <div className='level has-text-centered'>
             <p className='level-item is-vertical-align'>
